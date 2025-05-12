cef528e7fe02fba47ee68048b89669b80a8dd036
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-05-12 15:35:33 +0500

    Refactor into ReactTS + ASP.NET

--- web-ieee/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace web_ieee.Models;
-
-public class ApplicationUser : IdentityUser
-{
-    // TODO: Add necessary columns here
-}
--- web-ieee/Models/NewsViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace web_ieee.ViewModels;
-
-public class NewsViewModel
-{
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public IFormFile PhotoFile { get; set; }
-}
