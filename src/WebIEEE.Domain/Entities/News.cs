4775a8626e782e5fd03bb4bded37976959bfea22
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-05-20 12:14:55 +0500

    Create News entity

---
+++ src/WebIEEE.Domain/Entities/News.cs
@@ -0,0 +1,9 @@
+﻿namespace WebIEEE.Domain.Entities;
+
+public class News : BaseEntity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Uri ImageLink { get; set; }
+    public string Author { get; set; }
+}
--- src/WebIEEE.Infrastructure/WebIEEEDbContext.cs
+++ src/WebIEEE.Infrastructure/WebIEEEDbContext.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebIEEE.Domain.Entities;
 
 namespace WebIEEE.Infrastructure;
 
 public class WebIeeeDbContext(DbContextOptions<WebIeeeDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<News> News { get; set; }
 }
